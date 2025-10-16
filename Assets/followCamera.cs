5feb7699e13e7386d862a506baf65bfe6928b906
mbl525 <mbl525@york.ac.uk>
2025-10-16 17:11:58 +0100

    practical 4.2

--- Assets/followCamera.cs
+++ Assets/followCamera.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 
 public class followCamera : MonoBehaviour
 {
+    public GameObject target;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        target = GameObject.Find ("Car");
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject target = GameObject.Find ("Car");
+        
         gameObject.transform.LookAt(target.transform);
     }
 }
